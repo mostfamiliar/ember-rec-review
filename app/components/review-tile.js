8e329d952f2128cdbfb84256c6dc92bd4a885bf2
torrence stratton <tlstratton@gmail.com>
2016-04-08 11:31:06 -0700

    add multiple sorting options

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortBy: 'rating',
   sortedReviews: Ember.computed.sort('reviews', 'sortDefinition'),
+  sortBy: 'rating',
   reverseSort: false,
   sortDefinition: Ember.computed('sortBy', 'reverseSort', function(){
     let sortOrder = this.get('reverseSort') ? 'desc' : 'asc';
